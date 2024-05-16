d563277e20855cc7aae33b5e05e84774846a0590
mdsifaterabbi <mdsifaterabbi641@gmail.com>
2024-05-16 12:44:23 +0600

    amazon tabcontents done admin

--- src/app/admin/home/tabcontext/page.jsx
+++ src/app/admin/home/tabcontext/page.jsx
@@ -1,15 +1,26 @@
 "use client";
 
 import { model } from "mongoose";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const HomeTabContentAdmin = () => {
+  const router = useRouter();
+
   const [isClient, setIsClient] = useState(false);
   const [amazonData, setAmazonData] = useState([]);
   const [webData, setWebData] = useState([]);
   const [digitalMarketingData, setDigitalmarketingData] = useState([]);
   const [graphicsData, setGraphicsData] = useState();
   const [modelID, setModelID] = useState("");
+  const [targetKey, setTargetkey] = useState("");
+  const [targetIndex, setTargetIndex] = useState();
+  // =========== Temporary state variables starts here for holding data=======================
+  const [amaHeading, setAmaHeading] = useState([]);
+  const [amaDescription, setAmaDescription] = useState([]);
+  const [amaImgSrc, setAmaImgSrc] = useState([]);
+  const [amaImgAlt, setAmaImgAlt] = useState([]);
+  // =========== Temporary state variables ended here for holding data========================
 
   useEffect(() => {
     const fetchID = async () => {
@@ -45,6 +56,12 @@ const HomeTabContentAdmin = () => {
 
         const myAmazonData = await amazonResponse.json();
         setAmazonData(myAmazonData);
+
+        //now setting default values in the state variables for form population
+        setAmaHeading(myAmazonData.map((item) => item.heading));
+        setAmaDescription(myAmazonData.map((item) => item.description));
+        setAmaImgSrc(myAmazonData.map((item) => item.imgSrc));
+        setAmaImgAlt(myAmazonData.map((item) => item.imgAlt));
 
         //fetching webDevelopment data
         const webResponse = await fetch(
@@ -94,11 +111,51 @@ const HomeTabContentAdmin = () => {
     fetchData();
   }, [modelID]);
 
-  console.log(modelID);
+  //console.log(modelID);
   // console.log(amazonData);
   // console.log(webData);
   // console.log(digitalMarketingData);
   // console.log(graphicsData);
+  //console.log(amaHeading[2]);
+
+  const mySubmit = async (e) => {
+    e.preventDefault();
+    // console.log("Selected Key: ", targetKey, "Data Type: ", typeof targetKey);
+    // console.log(
+    //   "Selected Index: ",
+    //   targetIndex,
+    //   "Data Type: ",
+    //   typeof targetIndex
+    // );
+    // console.log("Updated Heading: ", amaHeading[targetIndex]);
+    // console.log("Model id: ", modelID);
+
+    const id = modelID;
+
+    const res = await fetch(`http://localhost:3000/api/service/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        newHeading: amaHeading[targetIndex],
+        newDescription: amaDescription[targetIndex],
+        newImgSrc: amaImgSrc[targetIndex],
+        newImgAlt: amaImgAlt[targetIndex],
+        category: targetKey,
+        index: targetIndex,
+      }),
+    });
+
+    if (!res.ok) {
+      throw new Error("Service model could not be updated");
+    }
+    if (res.ok) {
+      router.push("/admin/home/tabcontext");
+      router.refresh();
+      window.alert("Service model updated successfully");
+    }
+  };
 
   return (
     <div className="py-[100px] h-screen overflow-y-auto">
@@ -106,16 +163,136 @@ const HomeTabContentAdmin = () => {
         <div className="flex flex-row flex-wrap gap-2 my-[10px] justify-center">
           {amazonData?.map((amaItem, index) => (
             <div key={index}>
-              <div className="card w-[280px] min-h-[450px] max-h-[450px] overflow-y-auto bg-base-100 shadow-xl">
-                <div className="badge badge-primary">{amaItem?.category}</div>
-                <div className="card-body">
-                  <h2 className="card-title">{amaItem?.heading}</h2>
-                  <h3>{amaItem?.description}</h3>
-                  <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Update</button>
+              <form onSubmit={mySubmit} className="border-2 border-slate-400">
+                <div className="flex flex-col flex-wrap">
+                  <div className="badge badge-accent rounded-none h-[30px] ml-[5px] mt-[5px]">
+                    {amaItem?.category}
+                  </div>
+                  <div className="w-[98%] mx-auto">
+                    <label for="username" className="text-gray-600  font-bold">
+                      Heading:
+                    </label>
+                    {isClient ? (
+                      <textarea
+                        type="text"
+                        id="heading"
+                        name="heading"
+                        className="w-[98%] px-[5px] pt-[5px] max-h-[150px] border-none text-left bg-slate-600 text-white"
+                        value={amaHeading[index]}
+                        onChange={(e) => {
+                          const updatedHeading = amaHeading.map((item, i) =>
+                            i === index ? e.target.value : item
+                          );
+                          setAmaHeading(updatedHeading);
+                        }}
+                      />
+                    ) : (
+                      <div>
+                        <span className="loading loading-bars loading-xs"></span>
+                        <span className="loading loading-bars loading-sm"></span>
+                        <span className="loading loading-bars loading-md"></span>
+                        <span className="loading loading-bars loading-lg"></span>
+                      </div>
+                    )}
+                  </div>
+
+                  <div className="w-[98%] mx-auto">
+                    <label for="username" className="text-gray-600  font-bold">
+                      Description:
+                    </label>
+                    {isClient ? (
+                      <textarea
+                        type="text"
+                        id="description"
+                        name="description"
+                        className="w-[98%] px-[5px] pt-[5px] text-left max-h-[300px] text-[12px] border-none bg-slate-600 text-white"
+                        value={amaDescription[index]}
+                        onChange={(e) => {
+                          const updatedDescription = amaDescription.map(
+                            (item, i) => (i === index ? e.target.value : item)
+                          );
+                          setAmaDescription(updatedDescription);
+                        }}
+                      />
+                    ) : (
+                      <div>
+                        <span className="loading loading-bars loading-xs"></span>
+                        <span className="loading loading-bars loading-sm"></span>
+                        <span className="loading loading-bars loading-md"></span>
+                        <span className="loading loading-bars loading-lg"></span>
+                      </div>
+                    )}
+                  </div>
+
+                  <div className="w-[98%] mx-auto">
+                    <label for="username" className="text-gray-600 font-bold">
+                      Image Source:
+                    </label>
+                    {isClient ? (
+                      <textarea
+                        type="text"
+                        id="imgSrc"
+                        name="imgSrc"
+                        className="w-[98%] px-[5px] pt-[5px] text-left max-h-[100px] text-[12px] border-none bg-slate-600 text-white"
+                        value={amaImgSrc[index]}
+                        onChange={(e) => {
+                          const updatedImgSrc = amaImgSrc.map((item, i) =>
+                            i === index ? e.target.value : item
+                          );
+                          setAmaImgSrc(updatedImgSrc);
+                        }}
+                      />
+                    ) : (
+                      <div>
+                        <span className="loading loading-bars loading-xs"></span>
+                        <span className="loading loading-bars loading-sm"></span>
+                        <span className="loading loading-bars loading-md"></span>
+                        <span className="loading loading-bars loading-lg"></span>
+                      </div>
+                    )}
+                  </div>
+
+                  <div className="w-[98%] mx-auto">
+                    <label for="username" className="text-gray-600 font-bold">
+                      Image Alt:
+                    </label>
+                    {isClient ? (
+                      <textarea
+                        type="text"
+                        id="imgAlt"
+                        name="imgAlt"
+                        className="w-[98%] px-[5px] pt-[5px] text-left max-h-[100px] text-[12px] border-none bg-slate-600 text-white"
+                        value={amaImgAlt[index]}
+                        onChange={(e) => {
+                          const updatedImgAlt = amaImgAlt.map((item, i) =>
+                            i === index ? e.target.value : item
+                          );
+                          setAmaImgAlt(updatedImgAlt);
+                        }}
+                      />
+                    ) : (
+                      <div>
+                        <span className="loading loading-bars loading-xs"></span>
+                        <span className="loading loading-bars loading-sm"></span>
+                        <span className="loading loading-bars loading-md"></span>
+                        <span className="loading loading-bars loading-lg"></span>
+                      </div>
+                    )}
+                  </div>
+
+                  <div className="mx-auto my-[20px] text-center hover:cursor-pointer">
+                    <button
+                      onClick={() => {
+                        setTargetIndex(index);
+                        setTargetkey(amaItem?.category);
+                      }}
+                      className="btn bg-purple-500 text-white hover: cursor-pointer"
+                    >
+                      Update
+                    </button>
                   </div>
                 </div>
-              </div>
+              </form>
             </div>
           ))}
         </div>
@@ -138,7 +315,7 @@ const HomeTabContentAdmin = () => {
                   <h2 className="card-title">{webItem?.heading}</h2>
                   <h3>{webItem?.description}</h3>
                   <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Update</button>
+                    <button className="btn bg-sky-500">Update</button>
                   </div>
                 </div>
               </div>
@@ -166,7 +343,7 @@ const HomeTabContentAdmin = () => {
                   <h2 className="card-title">{marketingItem?.heading}</h2>
                   <h3>{marketingItem?.description}</h3>
                   <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Update</button>
+                    <button className="btn bg-sky-500">Update</button>
                   </div>
                 </div>
               </div>
@@ -194,7 +371,7 @@ const HomeTabContentAdmin = () => {
                   <h2 className="card-title">{graphicsItem?.heading}</h2>
                   <h3>{graphicsItem?.description}</h3>
                   <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Update</button>
+                    <button className="btn bg-sky-500">Update</button>
                   </div>
                 </div>
               </div>
@@ -209,25 +386,6 @@ const HomeTabContentAdmin = () => {
           <span className="loading loading-bars loading-lg"></span>
         </div>
       )}
-
-      {/* <div className="flex flex-row flex-wrap gap-2 my-[10px] justify-center">
-        {graphicsData?.map((graphicsItem, index) => (
-          <div key={index}>
-            <div className="card w-[280px] min-h-[450px] max-h-[450px] overflow-y-auto bg-base-100 shadow-xl">
-              <div className="badge badge-warning">
-                {graphicsItem?.category}
-              </div>
-              <div className="card-body">
-                <h2 className="card-title">{graphicsItem?.heading}</h2>
-                <h3>{graphicsItem?.description}</h3>
-                <div className="card-actions justify-end">
-                  <button className="btn btn-primary">Update</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div> */}
     </div>
   );
 };
