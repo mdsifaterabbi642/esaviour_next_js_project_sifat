f7112f1f6b7239d8068c8035c202962de0d70d93
mdsifaterabbi <mdsifaterabbi641@gmail.com>
2024-05-18 18:06:37 +0600

    client added and deletet and updated successfully

--- src/app/admin/home/homeclient/page.jsx
+++ src/app/admin/home/homeclient/page.jsx
@@ -1,4 +1,5 @@
 "use client";
+import { model } from "mongoose";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -11,13 +12,23 @@ const HomeClientAdmin = () => {
 
   const [selectedOperation, setSelectedOperation] = useState("");
 
-  // =======set temp state variables==========
+  // ======== state variables for adding client starts=========
+  const [addName, setAddName] = useState("");
+  const [addParagraph, setAddParagraph] = useState("");
+  const [addCompany, setAddCompany] = useState("");
+  const [addImgSource, setAddImgSource] = useState("");
+  const [addImgAlt, setAddImgAlt] = useState("");
+  const [clientAddStatus, setClientAddStatus] = useState(false);
+  const [clientDeleteStatus, setClientDeleteStatus] = useState(false);
+  // ======== state variables for adding client ends=========
+
+  // =======set temp state variables starts==========
   const [names, setNames] = useState([]);
   const [paragraphs, setParagraphs] = useState([]);
   const [companies, setCompanies] = useState([]);
   const [imgSources, setImgSources] = useState([]);
   const [imgAlts, setImgAlts] = useState([]);
-  // =================
+  // =================set temp state variables ends======
 
   useEffect(() => {
     const fetchClientData = async () => {
@@ -44,43 +55,35 @@ const HomeClientAdmin = () => {
     fetchClientData();
   }, []);
 
+  useEffect(() => {
+    const fetchClientData = async () => {
+      const res = await fetch("http://localhost:3000/api/homeclient", {
+        cache: "no-store",
+      });
+
+      if (!res.ok) {
+        throw new Error("Error occurred in fetchClientData");
+      }
+      const myClients = await res.json();
+      setData(myClients);
+      setModelId(myClients[0]._id);
+
+      //adding default values to the state variables to populate form fields
+      setNames(myClients[0]?.clientCard?.map((item) => item.name));
+      setParagraphs(myClients[0]?.clientCard?.map((item) => item.paragraph));
+      setCompanies(myClients[0]?.clientCard?.map((item) => item.company));
+      setImgSources(myClients[0]?.clientCard?.map((item) => item.imgSource));
+      setImgAlts(myClients[0]?.clientCard?.map((item) => item.imgAlt));
+
+      setIsClient(true);
+    };
+    fetchClientData();
+  }, [clientAddStatus, clientDeleteStatus]);
+
   //console.log(data[0]?.clientCard);
 
   const mySubmit = async (e) => {
     e.preventDefault();
-    // console.log("Clicked my submit");
-    // console.log("modelId: ", modelId);
-    // console.log("targetIndex: ", targetIndex);
-    // console.log("updatedName: ", names[targetIndex]);
-    // console.log("updatedParagraph: ", paragraphs[targetIndex]);
-    // console.log("updatedCompany: ", companies[targetIndex]);
-    // console.log("updatedImgSource: ", imgSources[targetIndex]);
-    // console.log("updatedImgAlt: ", imgAlts[targetIndex]);
-
-    // const res = await fetch(`http://localhost:3000/api/homeclient/${modelId}`, {
-    //   method: "PATCH",
-    //   headers: {
-    //     "Content-type": "application/json",
-    //   },
-    //   body: JSON.stringify({
-    //     modelId: modelId,
-    //     targetIndex: targetIndex,
-    //     updatedName: names[targetIndex],
-    //     updatedParagraph: paragraphs[targetIndex],
-    //     updatedCompany: companies[targetIndex],
-    //     updatedImgSource: imgSources[targetIndex],
-    //     updatedImgAlt: imgAlts[targetIndex],
-    //   }),
-    // });
-
-    // if (!res.ok) {
-    //   throw new Error("Client data couldn't be updated");
-    // }
-    // if (res.ok) {
-    //   router.push("/admin/home/homeclient");
-    //   router.refresh();
-    //   window.alert("homeclients model updated successfully");
-    // }
 
     console.log("clicked mysubmit");
 
@@ -120,13 +123,199 @@ const HomeClientAdmin = () => {
       console.log("Your selected operation is: ", selectedOperation);
       console.log("Model ID: ", modelId);
       console.log("target index: ", targetIndex);
+
+      const id = modelId;
+
+      const res = await fetch(`http://localhost:3000/api/homeclient/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+          targetIndex: targetIndex,
+        }),
+      });
+
+      if (!res.ok) {
+        throw new Error("Client couldn't be deleted");
+      }
+      if (res.ok) {
+        router.push("/admin/home/homeclient");
+        router.refresh();
+        window.alert("Client deleted successfully");
+        setClientDeleteStatus(true);
+      }
     }
+  };
+
+  const addClient = async (e) => {
+    e.preventDefault();
+
+    //validation of the fields
+    if (addName.length < 5) {
+      window.alert("Name character length should be more than 5");
+      return;
+    }
+    if (addParagraph.length < 15) {
+      window.alert("Paragraph character length should be more than 15");
+      return;
+    }
+
+    if (addCompany.length < 3) {
+      window.alert("Company character length should be more than 3");
+      return;
+    }
+
+    if (addImgSource.length === null) {
+      window.alert("Please add image source");
+      return;
+    }
+
+    if (addImgAlt.length === null) {
+      window.alert("Please add image alter tag");
+      return;
+    }
+
+    const res = await fetch(`http://localhost:3000/api/homeclient/${modelId}`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        addName: addName,
+        addParagraph: addParagraph,
+        addCompany: addCompany,
+        addImgSource: addImgSource,
+        addImgAlt: addImgAlt,
+      }),
+    });
+
+    if (!res.ok) {
+      throw new Error("New Client couldn't be added");
+    }
+
+    if (res.ok) {
+      router.push("/admin/home/homeclient");
+      router.refresh();
+      window.alert("New Client added successfully");
+      setClientAddStatus(true);
+      document.getElementById("addClient").close();
+    }
+
+    console.log("addClient function triggerred");
+    console.log("modelId: ", modelId);
+    console.log("addName: ", addName);
+    console.log("addParagraph: ", addParagraph);
+    console.log("addCompany: ", addCompany);
+    console.log("addImgSource: ", addImgSource);
+    console.log("addImgAlt: ", addImgAlt);
   };
 
   return (
     <>
+      {/* add client form */}
+      <dialog id="addClient" className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Add Client</h3>
+          <div>
+            <form onSubmit={addClient}>
+              <div className="flex flex-col flex-wrap">
+                <div className="w-[98%] mx-auto">
+                  <label for="name" className="text-gray-600  font-bold">
+                    Name:
+                  </label>
+                  <textarea
+                    type="text"
+                    id="name"
+                    name="name"
+                    placeholder="Client name"
+                    className="w-[98%] px-[5px] pt-[5px] h-[50px] min-h-[50px] max-h-[100px] border-none text-left bg-slate-600 text-white"
+                    value={addName}
+                    onChange={(e) => setAddName(e.target.value)}
+                    required={true}
+                  />
+                </div>
+                <div className="w-[98%] mx-auto">
+                  <label for="paragraph" className="text-gray-600  font-bold">
+                    Paragraph:
+                  </label>
+                  <textarea
+                    type="text"
+                    id="paragraph"
+                    name="paragraph"
+                    placeholder="Add statement or review"
+                    className="w-[98%] px-[5px] pt-[5px] h-[50px] min-h-[50px] max-h-[150px] border-none text-left bg-slate-600 text-white"
+                    value={addParagraph}
+                    onChange={(e) => setAddParagraph(e.target.value)}
+                    required={true}
+                  />
+                </div>
+                <div className="w-[98%] mx-auto">
+                  <label for="company" className="text-gray-600  font-bold">
+                    Company:
+                  </label>
+                  <textarea
+                    type="text"
+                    id="company"
+                    name="company"
+                    placeholder="company name"
+                    className="w-[98%] h-[50px] min-h-[50px] max-h-[70px] px-[5px] pt-[5px] border-none text-left bg-slate-600 text-white"
+                    value={addCompany}
+                    onChange={(e) => setAddCompany(e.target.value)}
+                    required={true}
+                  />
+                </div>
+                <div className="w-[98%] mx-auto">
+                  <label for="image" className="text-gray-600  font-bold">
+                    Image:
+                  </label>
+                  <textarea
+                    type="text"
+                    id="imgSource"
+                    name="imgSource"
+                    placeholder="Add image source link"
+                    className="w-[98%] px-[5px] pt-[5px] h-[50px] min-h-[50px] max-h-[100px] border-none text-left bg-slate-600 text-white"
+                    value={addImgSource}
+                    onChange={(e) => setAddImgSource(e.target.value)}
+                    required={true}
+                  />
+                </div>
+                <div className="w-[98%] mx-auto">
+                  <label for="imgAlt" className="text-gray-600  font-bold">
+                    Image Alt:
+                  </label>
+                  <textarea
+                    type="text"
+                    id="imgAlt"
+                    name="imgAlt"
+                    placeholder="Add image alter tag"
+                    className="w-[98%] px-[5px] pt-[5px] h-[50px] min-h-[50px] max-h-[100px] border-none text-left bg-slate-600 text-white"
+                    value={addImgAlt}
+                    onChange={(e) => setAddImgAlt(e.target.value)}
+                    required={true}
+                  />
+                </div>
+              </div>
+              <div className="text-center mt-[20px]">
+                <button className="btn btn-sm bg-green-500 text-white">
+                  Add
+                </button>
+              </div>
+            </form>
+          </div>
+          <div className="modal-action">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
       <div>
-        <button className="btn bg-green-500 text-white fixed right-[5%] top-[5%]">
+        <button
+          className="btn bg-green-500 text-white fixed right-[5%] top-[5%]"
+          onClick={() => document.getElementById("addClient").showModal()}
+        >
           Add Client
         </button>
       </div>
@@ -146,8 +335,8 @@ const HomeClientAdmin = () => {
                       </label>
                       <textarea
                         type="text"
-                        id="heading"
-                        name="heading"
+                        id="name"
+                        name="name"
                         className="w-[98%] px-[5px] pt-[5px] min-h-[50px] max-h-[150px] border-none text-left bg-slate-600 text-white"
                         value={names[index]}
                         onChange={(e) => {
@@ -204,10 +393,7 @@ const HomeClientAdmin = () => {
                     </div>
 
                     <div className="w-[98%] mx-auto">
-                      <label
-                        for="username"
-                        className="text-gray-600  font-bold"
-                      >
+                      <label for="image" className="text-gray-600  font-bold">
                         Image:
                       </label>
                       <textarea
--- src/app/api/homeclient/[id]/route.js
+++ src/app/api/homeclient/[id]/route.js
@@ -2,6 +2,57 @@ import { NextResponse } from "next/server";
 import connectDB from "@/utils/db";
 import HomeClient from "@/models/HomeClient";
 
+export const POST = async (request, { params }) => {
+  try {
+    const { id } = params;
+
+    const { addName, addParagraph, addCompany, addImgSource, addImgAlt } =
+      await request.json();
+
+    await connectDB();
+
+    try {
+      const existingHomeClient = await HomeClient.findOne({ _id: id });
+      if (!existingHomeClient) {
+        return new NextResponse("client can not be added to unknown index", {
+          status: 404,
+        });
+      }
+
+      // Add a new client to the clientCard array
+      existingHomeClient.clientCard.push({
+        name: addName,
+        paragraph: addParagraph,
+        company: addCompany,
+        imgSource: addImgSource,
+        imgAlt: addImgAlt,
+      });
+
+      // Save the updated document
+      await existingHomeClient.save();
+    } catch (error) {
+      return new NextResponse("client can not be added to unknown index", {
+        status: 404,
+      });
+    }
+
+    let responseBody = {
+      addName: addName,
+      addParagraph: addParagraph,
+      addCompany: addCompany,
+      addImgSource: addImgSource,
+      addImgAlt: addImgAlt,
+    };
+
+    return new NextResponse(JSON.stringify(responseBody), { status: 200 });
+  } catch (error) {
+    return new NextResponse(
+      "Sorry, Error occurred in /api/homeclient/id post request",
+      { status: 500 }
+    );
+  }
+};
+
 export const PATCH = async (request, { params }) => {
   try {
     await connectDB();
@@ -62,6 +113,50 @@ export const PATCH = async (request, { params }) => {
   } catch (error) {
     return new NextResponse(
       "Sorry, Error occurred in /api/homeclient/id patch request",
+      { status: 500 }
+    );
+  }
+};
+
+export const DELETE = async (request, { params }) => {
+  try {
+    const { id } = params;
+    const { targetIndex } = await request.json();
+
+    await connectDB();
+    let existingHomeClient;
+    try {
+      existingHomeClient = await HomeClient.findOne({ _id: id });
+    } catch (error) {
+      return new NextResponse("Client not found to delete", { status: 404 });
+    }
+
+    let responseBody;
+
+    if (existingHomeClient !== null) {
+      if (
+        targetIndex >= 0 &&
+        targetIndex < existingHomeClient?.clientCard.length
+      ) {
+        existingHomeClient.clientCard.splice(targetIndex, 1);
+      } else {
+        return new NextResponse("Invalid target index", { status: 400 });
+      }
+    }
+
+    // Save the updated document
+    await existingHomeClient.save();
+
+    responseBody = {
+      _id: id,
+      targetIndex: targetIndex,
+      message: "Client deleted successfully",
+    };
+
+    return new NextResponse(JSON.stringify(responseBody), { status: 200 });
+  } catch (error) {
+    return new NextResponse(
+      "Sorry, Error occurred in /api/homeclient/id delete request",
       { status: 500 }
     );
   }
