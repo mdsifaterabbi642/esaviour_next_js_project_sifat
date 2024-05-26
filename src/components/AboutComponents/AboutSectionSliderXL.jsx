de4f7f0de403ce425e08c4a0f57ad3c9765a7ada
mdsifaterabbi <mdsifaterabbi641@gmail.com>
2024-05-26 13:00:52 +0600

    FAQ section of about admin done

--- src/components/AboutComponents/AboutSectionSliderXL.jsx
+++ src/components/AboutComponents/AboutSectionSliderXL.jsx
@@ -17,7 +17,6 @@ const AboutSectionSliderXL = () => {
     autoplaySpeed: 3000,
     cssEase: "linear",
     slidesToScroll: 1,
-    
   };
   const settings_2 = {
     className: "center",
@@ -32,95 +31,104 @@ const AboutSectionSliderXL = () => {
     cssEase: "linear",
     slidesToScroll: 1,
     rtl: true,
-    
   };
+
+  const section5_slider1_Data = [
+    {
+      quantity: "500+",
+      heading: "Happy Clients",
+    },
+    {
+      quantity: "27+",
+      heading: "Brains to Support",
+    },
+    {
+      quantity: "100+",
+      heading: "Website Developed",
+    },
+    {
+      quantity: "300+",
+      heading: "Package Design",
+    },
+    {
+      quantity: "150+",
+      heading: "Product Design",
+    },
+    {
+      quantity: "50+",
+      heading: "MERN STACK",
+    },
+  ];
+
+  const section5_slider2_Data = [
+    {
+      quantity: "500+",
+      heading: "Happy Clients",
+    },
+    {
+      quantity: "27+",
+      heading: "Brains to Support",
+    },
+    {
+      quantity: "100+",
+      heading: "Website Developed",
+    },
+    {
+      quantity: "300+",
+      heading: "Package Design",
+    },
+    {
+      quantity: "150+",
+      heading: "Product Design",
+    },
+    {
+      quantity: "750+",
+      heading: "Amazon Consultancy",
+    },
+  ];
 
   return (
     <>
       <div className="slider-container w-[100vw] mx-auto">
         <Slider {...settings}>
-          <div className="w-[390px] h-[350px] mySliderHover">
-            <div className="w-[90%] h-[90%] border-[2px] border-black rounded-xl mx-auto my-[20px]">
-              <div className="relative top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2">
-                <p className="text-[40px] text-center pl-[10px] text-[#40b0fd] font-thin italic">
-                  500+
-                </p>
-                <p className="font-bold text-[18px] text-center">
-                  Happy Clients
-                </p>
+          {section5_slider1_Data.map((item, index) => (
+            <div key={index} className="w-[390px] h-[350px] mySliderHover">
+              <div className="w-[90%] h-[90%] border-[2px] border-black rounded-xl mx-auto my-[20px]">
+                <div className="relative top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2">
+                  <h1 className="text-[40px] text-center pl-[10px] text-[#40b0fd] font-thin italic">
+                    {item.quantity}
+                  </h1>
+                  <h1 className="font-bold text-[18px] text-center">
+                    {item.heading}
+                  </h1>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="w-[390px] h-[350px] mySliderHover">
-            <div className="w-[90%] h-[90%] border-[2px] border-black rounded-xl mx-auto my-[20px]">
-              <div className="relative top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2">
-                <p className="text-[40px] text-center pl-[10px] text-[#40b0fd] font-thin italic">
-                  500+
-                </p>
-                <p className="font-bold text-[18px] text-center">
-                  Happy Clients
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="w-[390px] h-[350px] mySliderHover">
-            <div className="w-[90%] h-[90%] border-[2px] border-black rounded-xl mx-auto my-[20px]">
-              <div className="relative top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2">
-                <p className="text-[40px] text-center pl-[10px] text-[#40b0fd] font-thin italic">
-                  27+
-                </p>
-                <p className="font-bold text-[18px] text-center">
-                  Brains to Support
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="w-[390px] h-[350px] mySliderHover">
-            <div className="w-[90%] h-[90%] border-[2px] border-black rounded-xl mx-auto my-[20px]">
-              <div className="relative top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2">
-                <p className="text-[40px] text-center pl-[10px] text-[#40b0fd] font-thin italic">
-                  100+
-                </p>
-                <p className="font-bold text-[18px] text-center">
-                  Website Developed
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="w-[390px] h-[350px] mySliderHover">
-            <div className="w-[90%] h-[90%] border-[2px] border-black rounded-xl mx-auto my-[20px]">
-              <div className="relative top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2">
-                <p className="text-[40px] text-center pl-[10px] text-[#40b0fd] font-thin italic">
-                  300+
-                </p>
-                <p className="font-bold text-[18px] text-center">
-                  Package Design
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="w-[390px] h-[350px] mySliderHover">
-            <div className="w-[90%] h-[90%] border-[2px] border-black rounded-xl mx-auto my-[20px]">
-              <div className="relative top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2">
-                <p className="text-[40px] text-center pl-[10px] text-[#40b0fd] font-thin italic">
-                  150+
-                </p>
-                <p className="font-bold text-[18px] text-center">
-                  Product Design
-                </p>
-              </div>
-            </div>
-          </div>
+          ))}
         </Slider>
       </div>
       {/* =========== Slider 2 ============= */}
       <div className="slider-container w-[80vw] mx-auto">
         <Slider {...settings_2}>
-          <div className="w-[390px] h-[350px] mySliderHover">
+          {section5_slider2_Data.map((item, index) => (
+            <div key={index} className="w-[390px] h-[350px] mySliderHover">
+              <div className="w-[90%] h-[90%] border-[2px] border-black rounded-xl mx-auto my-[20px]">
+                <div className="relative top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2">
+                  <h1 className="text-[40px] text-center pl-[10px] text-[#40b0fd] font-thin italic">
+                    {item.quantity}
+                  </h1>
+                  <h1 className="font-bold text-[18px] text-center">
+                    {item.heading}
+                  </h1>
+                </div>
+              </div>
+            </div>
+          ))}
+          {/* <div className="w-[390px] h-[350px] mySliderHover">
             <div className="w-[90%] h-[90%] border-[2px] border-black rounded-xl mx-auto my-[20px]">
               <div className="relative top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2">
                 <p className="text-[40px] text-center pl-[10px] text-[#40b0fd] font-thin italic">
-                  500+
+                  500+ S 2.1
                 </p>
                 <p className="font-bold text-[18px] text-center">
                   Happy Clients
@@ -132,7 +140,7 @@ const AboutSectionSliderXL = () => {
             <div className="w-[90%] h-[90%] border-[2px] border-black rounded-xl mx-auto my-[20px]">
               <div className="relative top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2">
                 <p className="text-[40px] text-center pl-[10px] text-[#40b0fd] font-thin italic">
-                  27+
+                  27+ S 2.2
                 </p>
                 <p className="font-bold text-[18px] text-center">
                   Brains to Support
@@ -144,7 +152,7 @@ const AboutSectionSliderXL = () => {
             <div className="w-[90%] h-[90%] border-[2px] border-black rounded-xl mx-auto my-[20px]">
               <div className="relative top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2">
                 <p className="text-[40px] text-center pl-[10px] text-[#40b0fd] font-thin italic">
-                  100+
+                  100+ S 2.3
                 </p>
                 <p className="font-bold text-[18px] text-center">
                   Website Developed
@@ -156,7 +164,7 @@ const AboutSectionSliderXL = () => {
             <div className="w-[90%] h-[90%] border-[2px] border-black rounded-xl mx-auto my-[20px]">
               <div className="relative top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2">
                 <p className="text-[40px] text-center pl-[10px] text-[#40b0fd] font-thin italic">
-                  300+
+                  300+ S 2.4
                 </p>
                 <p className="font-bold text-[18px] text-center">
                   Package Design
@@ -168,14 +176,14 @@ const AboutSectionSliderXL = () => {
             <div className="w-[90%] h-[90%] border-[2px] border-black rounded-xl mx-auto my-[20px]">
               <div className="relative top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2">
                 <p className="text-[40px] text-center pl-[10px] text-[#40b0fd] font-thin italic">
-                  150+
+                  150+ S 2.5
                 </p>
                 <p className="font-bold text-[18px] text-center">
                   Product Design
                 </p>
               </div>
             </div>
-          </div>
+          </div> */}
         </Slider>
       </div>
     </>
--- src/components/admin-components/aboutAdminComponents/FAQAboutAdmin.jsx
+++ src/components/admin-components/aboutAdminComponents/FAQAboutAdmin.jsx
@@ -15,6 +15,13 @@ const FAQAboutAdmin = () => {
   const [answer, setAnswer] = useState([]);
   const [targetIndex, setTargetIndex] = useState();
   const [targetSection, setTargetSection] = useState("");
+  const [selectedAction, setSelectedAction] = useState("");
+  // state variables for adding FAQ
+  const [newQuestion, setNewQuestion] = useState("");
+  const [newAnswer, setNewAnswer] = useState("");
+  //==========
+  const [addFaqStatus, setAddFaqStatus] = useState(false);
+  const [deletionStatus, setDeletionStatus] = useState(false);
 
   useEffect(() => {
     const getFAQAboutAdminData = async () => {
@@ -36,6 +43,27 @@ const FAQAboutAdmin = () => {
     getFAQAboutAdminData();
     setIsClient(true);
   }, []);
+
+  useEffect(() => {
+    const getFAQAboutAdminData = async () => {
+      const res = await fetch("http://localhost:3000/api/about", {
+        cache: "no-store",
+      });
+      if (!res.ok) {
+        throw new Error("Failed to fetch data");
+      }
+      const myJsonData = await res.json();
+      setData(myJsonData);
+      //adding default values to the form through state variable
+      setImgSource(myJsonData[0]?.AboutFAQSection[0]?.FAQ_ImgSource);
+      setImgAlt(myJsonData[0]?.AboutFAQSection[0]?.FAQ_ImgAlt);
+      setSubtitle(myJsonData[0]?.AboutFAQSection[0]?.FAQ_SubTitle);
+      setQuestion(myJsonData[0]?.FAQLoop.map((item, i) => item.question));
+      setAnswer(myJsonData[0]?.FAQLoop.map((item, i) => item.answer));
+    };
+    getFAQAboutAdminData();
+    setIsClient(true);
+  }, [addFaqStatus, deletionStatus]);
 
   const faqSubmit1 = async (e) => {
     e.preventDefault();
@@ -85,39 +113,127 @@ const FAQAboutAdmin = () => {
     // console.log("Question: ", question);
     // console.log("Answer: ", answer);
 
-    const decision = window.prompt(
-      "Type `update 100 on 100` to update or type `cancel` to cancel the operation"
-    );
+    if (selectedAction === "update") {
+      const decision = window.prompt(
+        "Type `update 100 on 100` to update or type `cancel` to cancel the operation"
+      );
 
-    if (decision === "update 100 on 100") {
-      console.log("You choosed to update");
-      const id = data[0]?._id;
+      if (decision === "update 100 on 100") {
+        console.log("You choosed to update");
+        const id = data[0]?._id;
 
-      const res = await fetch(`http://localhost:3000/api/about/update/${id}`, {
-        method: "PATCH",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify({
-          question,
-          answer,
-          targetIndex,
-          targetSection,
-        }),
-      });
+        const res = await fetch(
+          `http://localhost:3000/api/about/update/${id}`,
+          {
+            method: "PATCH",
+            headers: {
+              "Content-type": "application/json",
+            },
+            body: JSON.stringify({
+              question,
+              answer,
+              targetIndex,
+              targetSection,
+            }),
+          }
+        );
 
-      if (!res.ok) {
-        throw new Error("About data of About page couldn't be updated");
+        if (!res.ok) {
+          throw new Error("About data of About page couldn't be updated");
+        }
+        if (res.ok) {
+          router.push("/admin/about");
+          router.refresh();
+          window.alert("About Model updated successfully");
+        }
+      } else if (decision === "cancel") {
+        console.log("You choosed to cancel the operation");
+      } else {
+        console.log("Invalid request");
       }
-      if (res.ok) {
-        router.push("/admin/about");
-        router.refresh();
-        window.alert("About Model updated successfully");
+    }
+    if (selectedAction === "delete") {
+      const confirm = window.prompt(
+        "Type `delete this FAQ` to delete or type `cancel` to cancel the operation"
+      );
+      if (confirm === "delete this FAQ") {
+        // console.log("You choose to delete");
+        // console.log("TargetSection: ", targetSection, "type of: ", typeof targetSection);
+        // console.log("TargetIndex: ", targetIndex, "type of: ", typeof targetIndex);
+
+        console.log("You choosed to delete");
+        const id = data[0]?._id;
+
+        const res = await fetch(`http://localhost:3000/api/about/faq/${id}`, {
+          method: "DELETE",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify({
+            targetIndex: targetIndex,
+            targetSection: targetSection,
+          }),
+        });
+
+        if (!res.ok) {
+          throw new Error("FAQ data of About page couldn't be deleted");
+        }
+        if (res.ok) {
+          router.push("/admin/about");
+          router.refresh();
+          window.alert("FAQ of About Model deleted successfully");
+        }
+        setDeletionStatus(true);
+      } else if (confirm === "cancel") {
+        console.log("You have cancelled the operation");
+      } else {
+        console.log("Invalid request");
       }
-    } else if (decision === "cancel") {
-      console.log("You choosed to cancel the operation");
-    } else {
-      console.log("Invalid request");
+    }
+  };
+
+  const addFAQ = async (e) => {
+    try {
+      e.preventDefault();
+
+      const confirm = window.prompt(
+        "Are you sure to add this? Type `yes i am sure` to proceed or type `cancel` to cancel the operation"
+      );
+
+      if (confirm === "yes i am sure") {
+        // console.log("Clicked addFAQ function");
+        // console.log("newQuestion: ", newQuestion);
+        // console.log("newAnswer: ", newAnswer);
+
+        const id = data[0]?._id;
+
+        const res = await fetch(`http://localhost:3000/api/about/faq/${id}`, {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify({
+            newQuestion: newQuestion,
+            newAnswer: newAnswer,
+          }),
+        });
+
+        if (!res.ok) {
+          throw new Error("About data of About page couldn't be updated");
+        }
+        if (res.ok) {
+          router.push("/admin/about");
+          router.refresh();
+          window.alert("About Model FAQ added successfully");
+        }
+        setAddFaqStatus(true);
+      } else if (confirm === "cancel") {
+        console.log("You cancelled the operation");
+      } else {
+        console.log("Invalid request");
+      }
+    } catch (error) {
+      throw new Error("Error occurred in addFAQ function");
     }
   };
 
@@ -228,8 +344,17 @@ const FAQAboutAdmin = () => {
         <div>Loading</div>
       )}
 
+      <div className="mt-[50px] relative left-[90%] top-0">
+        <button
+          onClick={() => document.getElementById("FAQ__modal").showModal()}
+          className="btn btn-sm bg-green-500"
+        >
+          Add
+        </button>
+      </div>
+
       {isClient ? (
-        <div className="flex flex-wrap flex-row gap-2">
+        <div className="flex flex-wrap flex-row gap-2 justify-center mt-[20px]">
           {data[0]?.FAQLoop?.map((item, index) => (
             <div key={index}>
               <div className="w-[300px] bg-base-100 shadow-xl">
@@ -301,16 +426,31 @@ const FAQAboutAdmin = () => {
                         )}
                       </div>
 
-                      <div className="mx-auto my-[20px] text-center hover:cursor-pointer">
-                        <button
-                          onClick={() => {
-                            setTargetSection("FAQLoop");
-                            setTargetIndex(index);
-                          }}
-                          className="btn btn-sm bg-[#000080] text-white hover:bg-orange-500 hover: cursor-pointer"
-                        >
-                          Update
-                        </button>
+                      <div className=" my-[20px] flex justify-between mx-[25px]">
+                        <div>
+                          <button
+                            onClick={() => {
+                              setTargetSection("FAQLoop");
+                              setTargetIndex(index);
+                              setSelectedAction("update");
+                            }}
+                            className="btn btn-sm bg-[#000080] text-white hover:bg-orange-500 hover: cursor-pointer"
+                          >
+                            Update
+                          </button>
+                        </div>
+                        <div>
+                          <button
+                            onClick={() => {
+                              setTargetSection("FAQLoop");
+                              setTargetIndex(index);
+                              setSelectedAction("delete");
+                            }}
+                            className="btn btn-sm bg-red-500 text-white hover:bg-red-700 hover: cursor-pointer"
+                          >
+                            Delete
+                          </button>
+                        </div>
                       </div>
                     </div>
                   </form>
@@ -322,6 +462,59 @@ const FAQAboutAdmin = () => {
       ) : (
         <div>Loading</div>
       )}
+
+      <dialog id="FAQ__modal" className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Add FAQ</h3>
+          <div>
+            <form onSubmit={addFAQ}>
+              <div className="flex flex-col flex-wrap">
+                <div className="w-[98%] mx-auto">
+                  <label for="question" className="text-gray-600  font-bold">
+                    Question:
+                  </label>
+                  <textarea
+                    type="text"
+                    id="question"
+                    name="question"
+                    placeholder="add question"
+                    className="w-[98%] px-[5px] pt-[5px] h-[50px] min-h-[50px] max-h-[100px] border-none text-left bg-slate-600 text-white"
+                    required={true}
+                    value={newQuestion}
+                    onChange={(e) => setNewQuestion(e.target.value)}
+                  />
+                </div>
+                <div className="w-[98%] mx-auto">
+                  <label for="answer" className="text-gray-600  font-bold">
+                    Answer:
+                  </label>
+                  <textarea
+                    type="text"
+                    id="answer"
+                    name="answer"
+                    placeholder="add answer"
+                    className="w-[98%] px-[5px] pt-[5px] h-[50px] min-h-[50px] max-h-[200px] border-none text-left bg-slate-600 text-white"
+                    required={true}
+                    value={newAnswer}
+                    onChange={(e) => setNewAnswer(e.target.value)}
+                  />
+                </div>
+              </div>
+              <div className="text-center mt-[20px]">
+                <button className="btn btn-sm bg-green-500 text-white">
+                  Add Item
+                </button>
+              </div>
+            </form>
+          </div>
+          <div className="modal-action">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
     </div>
   );
 };
