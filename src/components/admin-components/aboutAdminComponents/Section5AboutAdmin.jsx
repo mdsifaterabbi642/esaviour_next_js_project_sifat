6228424fdd0995932fac3e34be828b336b478a0a
mdsifaterabbi <mdsifaterabbi641@gmail.com>
2024-05-26 18:16:31 +0600

    section5 slider1 and slider2 for AboutAdmin page crud full done

--- src/components/admin-components/aboutAdminComponents/Section5AboutAdmin.jsx
+++ src/components/admin-components/aboutAdminComponents/Section5AboutAdmin.jsx
@@ -8,17 +8,22 @@ const Section5AboutAdmin = () => {
 
   const [data, setData] = useState([]);
   const [isClient, setIsClient] = useState(false);
-  const [quantity, setQuantity] = useState([]);
-  const [heading, setHeading] = useState([]);
+  const [quantity, setQuantity] = useState([]); //for section5_slider1 in About model
+  const [heading, setHeading] = useState([]); //for section5_slider1 in About model
+  const [quantity2, setQuantity2] = useState([]); //for section5_slider2 in About model
+  const [heading2, setHeading2] = useState([]); //for section5_slider2 in About model
   const [action, setAction] = useState("");
   const [targetSection, setTargetSection] = useState("");
   const [targetIndex, setTargetIndex] = useState();
   const [updateStatus, setUpdateStatus] = useState(false);
   const [deleteStatus, setDeleteStatus] = useState(false);
   const [addStatus, setAddStatus] = useState(false);
+  const [addStatus2, setAddStatus2] = useState(false);
   // state variables to add data
-  const [newHeading, setNewHeading] = useState("");
-  const [newQuantity, setNewQuantity] = useState("");
+  const [newHeading, setNewHeading] = useState(""); //for section5_slider1 in About model
+  const [newQuantity, setNewQuantity] = useState(""); //for section5_slider1 in About model
+  const [newHeading2, setNewHeading2] = useState(""); //for section5_slider2 in About model
+  const [newQuantity2, setNewQuantity2] = useState(""); //for section5_slider2 in About model
 
   useEffect(() => {
     const getSection5AboutData = async () => {
@@ -36,6 +41,12 @@ const Section5AboutAdmin = () => {
       );
       setHeading(
         myJsonData[0]?.section5_slider1?.map((item, index) => item.heading)
+      );
+      setQuantity2(
+        myJsonData[0]?.section5_slider2?.map((item, index) => item.quantity)
+      );
+      setHeading2(
+        myJsonData[0]?.section5_slider2?.map((item, index) => item.heading)
       );
     };
     getSection5AboutData();
@@ -59,10 +70,16 @@ const Section5AboutAdmin = () => {
       setHeading(
         myJsonData[0]?.section5_slider1?.map((item, index) => item.heading)
       );
+      setQuantity2(
+        myJsonData[0]?.section5_slider2?.map((item, index) => item.quantity)
+      );
+      setHeading2(
+        myJsonData[0]?.section5_slider2?.map((item, index) => item.heading)
+      );
     };
     getSection5AboutData();
     setIsClient(true);
-  }, [addStatus, updateStatus, deleteStatus]);
+  }, [addStatus, updateStatus, deleteStatus, addStatus2]);
 
   const section5_slider1_update_delete = async (e) => {
     e.preventDefault();
@@ -111,13 +128,6 @@ const Section5AboutAdmin = () => {
       }
     }
     if (action === "delete") {
-      //   console.log(
-      //     "You Clicked section5_slider1_update_delete function to delete"
-      //   );
-      //   console.log("TargetSection: ", targetSection);
-      //   console.log("TargetIndex: ", targetIndex);
-      //   console.log("Heading: ", heading);
-      //   console.log("Quantity: ", quantity);
       const decision2 = window.prompt(
         "Type `please delete this` to delete or type `cancel` to cancel the operation: "
       );
@@ -185,10 +195,125 @@ const Section5AboutAdmin = () => {
     }
   };
 
+  const section5_slider2_update_delete = async (e) => {
+    e.preventDefault();
+
+    if (action === "update") {
+      console.log(
+        "You Clicked section5_slider2_update_delete function to UPDATE"
+      );
+
+      const decision = window.prompt(
+        "Type `please update me` to update or type `cancel` to cancel the operation: "
+      );
+
+      if (decision === "please update me") {
+        const id = data[0]?._id;
+
+        const res = await fetch(
+          `http://localhost:3000/api/about/section5_slider2/${id}`,
+          {
+            method: "PATCH",
+            headers: {
+              "Content-type": "application/json",
+            },
+            body: JSON.stringify({
+              heading: heading2,
+              quantity: quantity2,
+              targetIndex: targetIndex,
+              targetSection: targetSection,
+            }),
+          }
+        );
+
+        if (!res.ok) {
+          throw new Error("section5_slider2 of About page couldn't be updated");
+        }
+        if (res.ok) {
+          router.push("/admin/about");
+          router.refresh();
+          window.alert("section5_slider2 of About Model updated successfully");
+        }
+        setUpdateStatus(true);
+      } else if (decision === "cancel") {
+        console.log("You cancelled the operation");
+      } else {
+        console.log("Invalid request");
+      }
+    }
+    if (action === "delete") {
+      const decision2 = window.prompt(
+        "Type `please delete this` to delete or type `cancel` to cancel the operation: "
+      );
+
+      if (decision2 === "please delete this") {
+        const id = data[0]?._id;
+
+        const res = await fetch(
+          `http://localhost:3000/api/about/section5_slider2/${id}`,
+          {
+            method: "DELETE",
+            headers: {
+              "Content-type": "application/json",
+            },
+            body: JSON.stringify({
+              targetIndex: targetIndex,
+              targetSection: targetSection,
+            }),
+          }
+        );
+
+        if (!res.ok) {
+          throw new Error("section5_slider2 of About page couldn't be deleted");
+        }
+        if (res.ok) {
+          router.push("/admin/about");
+          router.refresh();
+          window.alert("section5_slider2 of About Model deleted successfully");
+        }
+        setDeleteStatus((prev) => !prev);
+      } else if (decision2 === "cancel") {
+        console.log("You cancelled the operation");
+      } else {
+        console.log("Invalid request");
+      }
+    }
+  };
+
+  const addSection5_slider2 = async (e) => {
+    e.preventDefault();
+    const id = data[0]?._id;
+    const res = await fetch(
+      `http://localhost:3000/api/about/section5_slider2/${id}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ heading: newHeading2, quantity: newQuantity2 }),
+      }
+    );
+
+    if (!res.ok) {
+      throw new Error(
+        "could not added data to section5_slider2 of About page in admin panel"
+      );
+    }
+    if (res.ok) {
+      setNewQuantity2("");
+      setNewHeading2("");
+      router.push("/admin/about");
+      window.alert("section5_slider2 of About Model Added successfully");
+      router.refresh();
+      setAddStatus2((prevValue) => !prevValue);
+    }
+  };
+
   return (
     <div>
+      {/* ====================================Start of Section5 Slider1 model field===================================================== */}
       <h1 className="text-center font-extrabold text-gray-500 text-[26px] pt-[50px] pb-[20px]">
-        Section: Section5 ⚙👇👇👇
+        Section: Section5(Slider1) ⚙👇👇👇
       </h1>
       <div className="relative left-[90%]">
         <button
@@ -204,7 +329,7 @@ const Section5AboutAdmin = () => {
         {data[0]?.section5_slider1.map((item, index) => (
           <div
             key={index}
-            className="w-[200px] bg-base-100 shadow-xl py-[20px] px-[20px]"
+            className="w-[250px] bg-base-100 shadow-xl py-[20px] px-[20px]"
           >
             <form
               onSubmit={section5_slider1_update_delete}
@@ -216,7 +341,7 @@ const Section5AboutAdmin = () => {
                     for="heading"
                     className="text-gray-600  font-bold text-xl"
                   >
-                    Heading:
+                    Heading1:
                   </label>
                   {isClient ? (
                     <textarea
@@ -246,7 +371,7 @@ const Section5AboutAdmin = () => {
                     for="quantity"
                     className="text-gray-600  font-bold text-xl"
                   >
-                    Quantity:
+                    Quantity1:
                   </label>
                   {isClient ? (
                     <textarea
@@ -302,8 +427,126 @@ const Section5AboutAdmin = () => {
           </div>
         ))}
       </div>
+      {/* ====================================End of Section5 Slider1 model field===================================================== */}
+      {/* ====================================Start of Section5 Slider2 model field===================================================== */}
+      <h1 className="text-center font-extrabold text-gray-500 text-[26px] pt-[50px] pb-[20px]">
+        Section: Section5(Slider2) ⚙👇👇👇
+      </h1>
+      <div className="relative left-[90%]">
+        <button
+          onClick={() => {
+            document.getElementById("addItemssection5_slider2").showModal();
+          }}
+          className="btn btn-sm bg-green-500 hover:bg-green-700 text-white"
+        >
+          Add2
+        </button>
+      </div>
+      <div className="flex flex-wrap justify-center gap-2">
+        {data[0]?.section5_slider2.map((item, index) => (
+          <div
+            key={index}
+            className="w-[250px] bg-base-100 shadow-xl py-[20px] px-[20px]"
+          >
+            <form
+              onSubmit={section5_slider2_update_delete}
+              className="w-[100%] mx-auto border-2 border-slate-400"
+            >
+              <div className="flex flex-col flex-wrap">
+                <div className="w-[98%] mx-auto">
+                  <label
+                    for="heading"
+                    className="text-gray-600  font-bold text-xl"
+                  >
+                    Heading2:
+                  </label>
+                  {isClient ? (
+                    <textarea
+                      type="text"
+                      id="heading"
+                      name="heading"
+                      className="w-[98%] text-[12px] px-[5px] py-[20px] min-h-[100px] max-h-[150px] border-none text-left bg-slate-600 text-white rounded-md"
+                      value={heading2[index]}
+                      onChange={(e) => {
+                        const updatedHeading = heading2.map((item, i) =>
+                          i === index ? e.target.value : item
+                        );
+                        setHeading2(updatedHeading);
+                      }}
+                    />
+                  ) : (
+                    <div>
+                      <span className="loading loading-bars loading-xs"></span>
+                      <span className="loading loading-bars loading-sm"></span>
+                      <span className="loading loading-bars loading-md"></span>
+                      <span className="loading loading-bars loading-lg"></span>
+                    </div>
+                  )}
+                </div>
+                <div className="w-[98%] mx-auto">
+                  <label
+                    for="quantity"
+                    className="text-gray-600  font-bold text-xl"
+                  >
+                    Quantity2:
+                  </label>
+                  {isClient ? (
+                    <textarea
+                      type="text"
+                      id="quantity"
+                      name="quantity"
+                      className="w-[98%] text-[12px] px-[5px] py-[20px] min-h-[100px] max-h-[150px] border-none text-left bg-slate-600 text-white rounded-md"
+                      value={quantity2[index]}
+                      onChange={(e) => {
+                        const updatedQuantity = quantity2.map((item, i) =>
+                          i === index ? e.target.value : item
+                        );
+                        setQuantity2(updatedQuantity);
+                      }}
+                    />
+                  ) : (
+                    <div>
+                      <span className="loading loading-bars loading-xs"></span>
+                      <span className="loading loading-bars loading-sm"></span>
+                      <span className="loading loading-bars loading-md"></span>
+                      <span className="loading loading-bars loading-lg"></span>
+                    </div>
+                  )}
+                </div>
+                <div className="flex flex-row flex-wrap justify-between mx-[3px] my-[10px]">
+                  <div>
+                    <button
+                      onClick={() => {
+                        setAction("update");
+                        setTargetSection("section5_slider2");
+                        setTargetIndex(index);
+                      }}
+                      className="btn btn-sm bg-[#000080] text-white"
+                    >
+                      Update2
+                    </button>
+                  </div>
+                  <div>
+                    <button
+                      onClick={() => {
+                        setAction("delete");
+                        setTargetSection("section5_slider2");
+                        setTargetIndex(index);
+                      }}
+                      className="btn btn-sm bg-red-500 text-white"
+                    >
+                      Delete2
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        ))}
+      </div>
+      {/* ====================================End of Section5 Slider2 model field===================================================== */}
 
-      {/* add items in section2 service form */}
+      {/* add items in section5 slider1 form */}
       <dialog
         id="addItemssection5_slider1"
         className="modal modal-bottom sm:modal-middle"
@@ -341,6 +584,63 @@ const Section5AboutAdmin = () => {
                     required={true}
                     value={newQuantity}
                     onChange={(e) => setNewQuantity(e.target.value)}
+                  />
+                </div>
+              </div>
+              <div className="text-center mt-[20px]">
+                <button className="btn btn-sm bg-green-500 text-white">
+                  Add Item
+                </button>
+              </div>
+            </form>
+          </div>
+          <div className="modal-action">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
+
+      {/* add items in section5 slider2 form */}
+      <dialog
+        id="addItemssection5_slider2"
+        className="modal modal-bottom sm:modal-middle"
+      >
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Add Items: section5_slider2</h3>
+          <div>
+            <form onSubmit={addSection5_slider2}>
+              <div className="flex flex-col flex-wrap">
+                <div className="w-[98%] mx-auto">
+                  <label for="heading2" className="text-gray-600  font-bold">
+                    Heading2:
+                  </label>
+                  <textarea
+                    type="text"
+                    id="heading2"
+                    name="heading2"
+                    placeholder="add heading2"
+                    className="w-[98%] px-[5px] pt-[5px] h-[50px] min-h-[50px] max-h-[100px] border-none text-left bg-slate-600 text-white"
+                    required={true}
+                    value={newHeading2}
+                    onChange={(e) => setNewHeading2(e.target.value)}
+                  />
+                </div>
+                <div className="w-[98%] mx-auto">
+                  <label for="quantity2" className="text-gray-600  font-bold">
+                    Quantity:
+                  </label>
+                  <textarea
+                    type="text"
+                    id="quantity2"
+                    name="quantity2"
+                    placeholder="add quantity2"
+                    className="w-[98%] px-[5px] pt-[5px] h-[50px] min-h-[50px] max-h-[100px] border-none text-left bg-slate-600 text-white"
+                    required={true}
+                    value={newQuantity2}
+                    onChange={(e) => setNewQuantity2(e.target.value)}
                   />
                 </div>
               </div>
