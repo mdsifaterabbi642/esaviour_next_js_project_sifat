67604353372fc5c3c34041c4cb495a535fc7f593
mdsifaterabbi <mdsifaterabbi641@gmail.com>
2024-05-28 17:01:13 +0600

    blog CRUD api done

--- src/app/api/blog/myblog/route.js
+++ src/app/api/blog/myblog/route.js
@@ -139,3 +139,137 @@ export const POST = async (request) => {
     });
   }
 };
+
+export const PATCH = async (request, { params }) => {
+  try {
+    const {
+      targetIndex,
+      bannerTitle,
+      category,
+      imageSource,
+      alt,
+      width,
+      height,
+      bodyTitle,
+      bodyDescription,
+      blogDate,
+    } = await request.json();
+
+    await connectDB();
+
+    const allBlogs = await Blog.find();
+    const allCategories = await Category.find();
+    let targetBlog = allBlogs[0].article[targetIndex];
+
+    if (bannerTitle) {
+      //validating unique fields (bannerTitle)
+      const duplicateBannerTitle = allBlogs[0].article.find((item, index) => {
+        return index !== targetIndex && item.bannerTitle === bannerTitle;
+      });
+
+      if (duplicateBannerTitle) {
+        return new NextResponse("This bannerTitle already exists", {
+          status: 400,
+        });
+      }
+
+      allBlogs[0].article[targetIndex].bannerTitle = bannerTitle;
+    }
+    if (category) {
+      //verify valid category from Category model
+      //validating the category is present in the category Model or not
+      const searchValidCategory = allCategories[0].category.find((item) => {
+        return item.categoryName === category;
+      });
+
+      if (!searchValidCategory) {
+        return new NextResponse("Category not valid", {
+          status: 404,
+        });
+      }
+
+      allBlogs[0].article[targetIndex].category = category;
+
+      //also updating categorySlug based on categoryss
+      // Remove any leading/trailing spaces
+      const trimmedCategory = category.trim();
+      // Replace spaces with underscores
+      const slug = trimmedCategory.replace(/\s+/g, "_").toLowerCase();
+      allBlogs[0].article[targetIndex].categorySlug = slug;
+    }
+    if (imageSource) {
+      allBlogs[0].article[targetIndex].imageSource = imageSource;
+    }
+    if (alt) {
+      allBlogs[0].article[targetIndex].alt = alt;
+    }
+    if (width) {
+      allBlogs[0].article[targetIndex].width = width;
+    }
+    if (height) {
+      allBlogs[0].article[targetIndex].height = height;
+    }
+    if (bodyTitle) {
+      // validating unique bodyTitle
+      //validating unique fields (bannerTitle)
+      const duplicateBodyTitle = allBlogs[0].article.find((item, index) => {
+        return index !== targetIndex && item.bodyTitle === bodyTitle;
+      });
+
+      if (duplicateBodyTitle) {
+        return new NextResponse("This bodytitle already exists", {
+          status: 400,
+        });
+      }
+
+      allBlogs[0].article[targetIndex].bodyTitle = bodyTitle;
+    }
+    if (bodyDescription) {
+      allBlogs[0].article[targetIndex].bodyDescription = bodyDescription;
+    }
+    if (blogDate) {
+      allBlogs[0].article[targetIndex].blogDate = blogDate;
+    }
+
+    await allBlogs[0].save();
+
+    let responseBody = {
+      message: "You are in PATCH api in /api/blog/myblog/[id]",
+      targetBlog: targetBlog,
+    };
+
+    return new NextResponse(JSON.stringify(responseBody), { status: 200 });
+  } catch (error) {
+    return new NextResponse(
+      "Error occurred in PATCH method in /api/blog/myblog",
+      { status: 400 }
+    );
+  }
+};
+
+export const DELETE = async (request, { params }) => {
+  try {
+    const { targetIndex } = await request.json();
+    await connectDB();
+
+    const existingBlogs = await Blog.find();
+
+    //delete here
+    existingBlogs[0].article.splice(targetIndex, 1);
+
+    // Save the updated document
+    await existingBlogs[0].save();
+
+    let responseBody = {
+      targetIndex: targetIndex,
+      message: "Blog Deleted successfully",
+    };
+
+    return new NextResponse(JSON.stringify(responseBody), { status: 200 });
+  } catch (error) {
+    return new NextResponse(
+      "Error occurrent in DELETE api in /api/blog/myblog",
+      { status: 500 }
+    );
+  }
+};
--- src/models/Blog.js
+++ src/models/Blog.js
@@ -1,31 +1,5 @@
 import mongoose from "mongoose";
 
-// const blogSchema = new mongoose.Schema(
-//   {
-//     blogId: { type: Number, unique: true },
-//     bannerTitle: { type: String, unique: true, required: true },
-//     category: {
-//       categoryName: {
-//         type: String,
-//         ref: "Category",
-//         required: true, // Make categoryName required
-//       },
-//       categorySlug: {
-//         type: String,
-//         ref: "Category",
-//       },
-//     },
-//     imageSource: { type: String },
-//     alt: { type: String },
-//     width: { type: String },
-//     height: { type: String },
-//     bodyTitle: { type: String, required: true },
-//     bodyDescription: { type: String, required: true },
-//     blogDate: { type: String },
-//   },
-//   { timestamps: true, index: true }
-// );
-
 const blogSchema = new mongoose.Schema(
   {
     article: [
